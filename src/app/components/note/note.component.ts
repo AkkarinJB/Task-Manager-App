8d169c8cc2798700e68c621e08a21b8e5b1eb249
AkkarinJB <akkarinjujaiboon11087@gmail.com>
2025-03-10 03:22:49 +0700

    #Add component Home,About,Note,Contact

---
+++ src/app/components/note/note.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-note',
+  standalone:true,
+  imports: [RouterModule],
+  templateUrl: './note.component.html',
+  styleUrl: './note.component.css'
+})
+export class NoteComponent {
+
+}
